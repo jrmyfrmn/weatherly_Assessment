36312cec1d2278848e366f0181ba077e2969615d
jeremy.fermin <jeremy.fermin@accenture.com>
2022-09-06 16:21:28 +0800

    Updated September 6,2022

---
+++ Weatherly/Preview Content/CityData.swift
@@ -0,0 +1,8 @@
+//
+//  CityData.swift
+//  Weatherly
+//
+//  Created by jeremy.fermin on 9/6/22.
+//
+
+import Foundation
---
+++ Weatherly/View/Next5DaysView.swift
@@ -0,0 +1,20 @@
+//
+//  Next5DaysView.swift
+//  Weatherly
+//
+//  Created by jeremy.fermin on 9/6/22.
+//
+
+import SwiftUI
+
+struct Next5DaysView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Next5DaysView_Previews: PreviewProvider {
+    static var previews: some View {
+        Next5DaysView()
+    }
+}
